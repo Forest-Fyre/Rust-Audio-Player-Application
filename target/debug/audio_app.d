6c58396b18b987e40f90b0492b3b97127a4627b5
Joe McIntyre <joey.d.mcintyre04@gmail.com>
2024-11-15 17:50:14 +1100

    Added debugging folders to ignore

--- target/debug/audio_app.d
+++
@@ -1,1 +0,0 @@
-/home/ff43/audio_app/target/debug/audio_app: /home/ff43/audio_app/src/main.rs
--- target/debug/deps/audio_app-a4ec6c440bf6b30e.d
+++
@@ -1,5 +0,0 @@
-/home/ff43/audio_app/target/debug/deps/audio_app-a4ec6c440bf6b30e: src/main.rs
-
-/home/ff43/audio_app/target/debug/deps/audio_app-a4ec6c440bf6b30e.d: src/main.rs
-
-src/main.rs:
